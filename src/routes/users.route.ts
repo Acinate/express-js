41dd65ee6b4061dfc5e52681337c871aca29bc6c
Acinate <Acinate@hotmail.com>
2019-08-01 18:50:01 -0400

    added type checking to req, res

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,11 +1,11 @@
-import express = require("express");
 import { UserDatabase } from "./../database/user/user.database";
+import express, { Request, Response } from "express";
 const router = express.Router();
 const database = new UserDatabase();
 
-router.get('/list', (req, res) => {
-    const users = database.getUsers();
-    res.send(JSON.stringify(users));
+router.get("/list", (req: Request, res: Response) => {
+  const users = database.getUsers();
+  res.send(JSON.stringify(users));
 });
 
 module.exports = router;
