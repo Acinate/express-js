6fb120e36899bb936dbc2c38b6b20841c9661566
Acinate <Acinate@hotmail.com>
2019-09-11 14:39:55 -0400

    minor adjustments

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,6 @@ import * as homeController from "./controllers/home";
 // Create Express server
 const app = express();
 
-
 // Express configuration
 app.set("port", process.env.PORT || 3000);
 app.use(compression());
